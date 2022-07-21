2b76d479c2b375ee745f85a1e54f6564d6a3ffa7
Marina Kaeufer <marishutka@yahoo.com>
2022-07-20 21:17:52 -0400

    worked on general functionality

--- script.js
+++ script.js
@@ -3,28 +3,35 @@ window.addEventListener('load', (event) => {
 
     init();
   });
-
+// Score 
+let score = 0;
 // ID names 
 const startButtonName = "start_button";
 // Content
 const content = {
     startPage: `<h1>Welcome</h1><button id=${startButtonName}>Start Quiz</button>`,
-    highScore: `High Score Page`
+    highScore: `High Score Page`,
+    finalPage: `Final Page with score ${score}`,
 };
 // Elements
 const mainContentElement = document.getElementById('content');
 const timerElement = document.getElementById('timer');
 // Timer 
-var timeleft = 60;
-var timer; 
+const initialTime = 60;
+let timeleft = initialTime;
+let timer; 
 // Questions 
-
+const questions = [
+    {question: "Question 1", options: ["Option 1","Option 2","Option 3","Option 4"], answer: 0},
+    {question: "Question 2", options: ["Option 1","Option 2","Option 3","Option 4"], answer: 1}
+];
+let currentQuestion = 0;
 
 function init(){
     attachScoreEventListener();
     loadStartPageContent();
     attachStartButtonEventListener();
-    loadTimer();
+    loadInitialTimer();
 }
 
 function attachScoreEventListener(){
@@ -33,12 +40,12 @@ function attachScoreEventListener(){
 }
 
 function attachStartButtonEventListener(){
-    const startButtonElement = document.getElementById('content');
+    const startButtonElement = document.getElementById(startButtonName);
     startButtonElement.addEventListener ("click", startQuiz);
 }
 
-function loadTimer(){
-    timerElement.innerHTML = timeleft;
+function loadInitialTimer(){
+    timerElement.innerHTML = initialTime;
 }
 
 function loadStartPageContent(){
@@ -57,17 +64,82 @@ function displayHighScores(){
 function startTimer(){
     timer = setInterval(function(){
         if(timeleft <= 0){
+            console.log(`Time up`);
+            // TODO Stop timer
             clearInterval(timer);
+            // End Quiz and show results / score
+            endQuiz();
         }
         timerElement.innerHTML = timeleft;
         timeleft -= 1;
         }, 1000);
 }
 
-function loadQuestionsContent(){
-    
+function loadFinalPage(){
+    displayMainContent('finalPage');
 }
 
+function loadQuestionsContent(){
+    // Display current question text
+    const questionText = questions[currentQuestion]['question'];
+    const questionTextNode = document.createElement("div");
+    const textnode = document.createTextNode(questionText);
+    questionTextNode.appendChild(textnode);
+    
+    // Display current question options
+    const questionOptions = questions[currentQuestion]['options'];
+    const questionOptionsNode = document.createElement("div");
+    questionOptions.forEach((option,index) => {
+        const optionTextnode = document.createTextNode(option);
+        const questionOptionNode = document.createElement("button");
+        questionOptionNode.addEventListener("click", function () {
+            handleResponse(index);
+          });
+        questionOptionNode.appendChild(optionTextnode);
+        questionOptionsNode.appendChild(questionOptionNode);
+    })
+    
+
+    // Append all to main content element
+    mainContentElement.innerHTML = "";
+    mainContentElement.appendChild(questionTextNode);
+    mainContentElement.appendChild(questionOptionsNode);
+}
+
+
+function handleResponse(answer){
+    console.log(`For question ${currentQuestion} you responded with ${answer}`);
+    // Check if response is false, if so subtract from time
+    if(answer !== questions[currentQuestion].answer){
+        console.log(`Incorrect response`);
+        subtractTime();
+    } else {
+        score += 1;
+        console.log(`Correct response`);
+    }
+    // Go to next question of show final page with results / score
+    if(currentQuestion + 1 < questions.length){
+        currentQuestion =+ 1;
+        loadQuestionsContent();
+    } else {
+        endQuiz();
+    }
+}
+
+function subtractTime(){
+    timeleft -= 10;
+}
+
+function endQuiz(){
+    loadFinalPage();
+    resetQuizParams();
+}
+
+function resetQuizParams(){
+    timeleft = initialTime;
+    currentQuestion = 0;
+    score = 0;
+}
 
 function displayMainContent(contentName){
     mainContentElement.innerHTML = content[contentName];
